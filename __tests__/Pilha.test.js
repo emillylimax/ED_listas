ae3e8581fc908f85806b612281b1f128d617d33b
Emilly <emillylimaxx@gmail.com>
2025-04-06 06:31:13 -0300

    Lista 1 - Até questão 2

--- __tests__/Pilha.test.js
+++ __tests__/Pilha.test.js
@@ -1,41 +1,103 @@
-import Pilha from "../src/Pilha";
+// import Pilha from "../src/Pilha";
 
 
-let p;
+// let p;
 
-beforeEach(() => {
-    p = new Pilha();
-})
+// beforeEach(() => {
+//     p = new Pilha();
+// })
 
-test("Se a pilha acabou de ser criada, ela deve estar vazia.", () => {
-    expect(p.isEmpty()).toBe(true);
-})
+// test("Se a pilha acabou de ser criada, ela deve estar vazia.", () => {
+//     expect(p.isEmpty()).toBe(true);
+// })
 
-test("Se eu inserir um elemento, a pilha não pode estar mais vazia.", () => {
-    p.push(1);
-    expect(p.isEmpty()).toBe(false);
-    expect(p.top()).toBe(1);
-})
+// test("Se eu inserir um elemento, a pilha não pode estar mais vazia.", () => {
+//     p.push(1);
+//     expect(p.isEmpty()).toBe(false);
+//     expect(p.top()).toBe(1);
+// })
 
-test("Se eu inserir 5 elementos a pilha vai está cheia", () => {
-    p.push(1);
-    p.push(2);
-    p.push(3);
-    p.push(4);
-    p.push(5);
-    expect(p.isFull()).toBe(true);
-    expect(p.isEmpty()).toBe(false);
-    expect(p.top()).toBe(5);
-    expect(p.length()).toBe(5);
-})
-test("Quem está no topo?", () => {
-    p.push(1);
-    p.push(2);
-    p.push(3);
-    p.pop();
-    p.pop();
-    expect(p.top()).toBe(1);
-    p.pop();
-    expect(p.isEmpty()).toBe(true);
-    expect(p.length()).toBe(0);
-})
+// test("Se eu inserir 5 elementos a pilha vai está cheia", () => {
+//     p.push(1);
+//     p.push(2);
+//     p.push(3);
+//     p.push(4);
+//     p.push(5);
+//     expect(p.isFull()).toBe(true);
+//     expect(p.isEmpty()).toBe(false);
+//     expect(p.top()).toBe(5);
+//     expect(p.length()).toBe(5);
+// })
+// test("Quem está no topo?", () => {
+//     p.push(1);
+//     p.push(2);
+//     p.push(3);
+//     p.pop();
+//     p.pop();
+//     expect(p.top()).toBe(1);
+//     p.pop();
+//     expect(p.isEmpty()).toBe(true);
+//     expect(p.length()).toBe(0);
+// })
+
+
+
+// //Lista 1: Questão 1
+// import { inverte } from "../src/Pilha";
+
+// test('Inverte a palavra ABACAXI', () => {
+//     expect(inverte('ABACAXI')).toBe('IXACABA');
+// });
+
+// // ____________________________________________
+
+// //Lista 1: Questão 2
+// import DuasPilhas from "../src/Pilha";
+
+// let pilhas;
+
+// beforeEach(() => {
+//     pilhas = new DuasPilhas(10);
+// });
+
+// test ('Empilhar e desempilhar A', () => {
+//     pilhas.empilhaA(1);
+//     pilhas.empilhaA(2);
+//     expect(pilhas.desempilhaA()).toBe(2);
+//     expect(pilhas.desempilhaA()).toBe(1);
+//     expect(pilhas.eVaziaA()).toBe(true);
+// });
+
+// test ('Empilhar e desempilhar B', () => {
+//     pilhas.empilhaB(10);
+//     pilhas.empilhaB(30);
+//     expect(pilhas.desempilhaB()).toBe(30);
+//     expect(pilhas.desempilhaB()).toBe(10);
+//     expect(pilhas.eVaziaB()).toBe(true);
+// });
+
+// test ('Empilhar em ambas pilhas', () => {
+//     pilhas.empilhaA(1);
+//     pilhas.empilhaA(2);
+//     pilhas.empilhaB(10);
+//     pilhas.empilhaB(20);
+
+//     expect(pilhas.desempilhaA()).toBe(2);
+//     expect(pilhas.desempilhaB()).toBe(20);
+//     expect(pilhas.desempilhaA()).toBe(1);
+//     expect(pilhas.desempilhaB()).toBe(10);
+// });
+
+// test('Empilhar pilha cheia', () => {
+//     for (let i = 0; i <5; i++){
+//         pilhas.empilhaA(i);
+//         pilhas.empilhaB(i+10);
+//     }
+
+//     expect(() => pilhas.empilhaA(99)).toThrow('Pilha Cheia');
+//     expect(() => pilhas.empilhaB(10)).toThrow('Pilha Cheia');
+// });
+
+// // ____________________________________________
+
+//Lista 1: Questão 3
--- src/Pilha.js
+++ src/Pilha.js
@@ -1,39 +1,147 @@
-class Pilha {
-    constructor(size = 5) {
-        this.dados = [];
-        this.size = size;
-        this.topo = 0;
-    }
-    push(elemento) {
-        if (this.isFull()) {
-            throw new Error("Stack Overflow");
-        }
-        this.dados[this.topo] = elemento;
-        this.topo++;
-    }
-    pop() {
-        if (this.isEmpty()) {
-            throw new Error("Stack underflow");
-        }
-        this.topo--;
-    }
-    top() {
-        if (!this.isEmpty()) {
-            return this.dados[this.topo - 1];
-        }
-    }
-    isEmpty() {
-        return this.length() === 0;
-    }
-    isFull() {
-        return this.length() === this.size;
-    }
-    toString() { }
-    clear() {
-        this.topo = 0;
-    }
-    length() {
-        return this.topo;
-    }
-}
-export default Pilha;
+// class Pilha {
+//     constructor(size = 5) {
+//         this.dados = [];
+//         this.size = size;
+//         this.topo = 0;
+//     }
+//     push(elemento) {
+//         if (this.isFull()) {
+//             throw new Error("Stack Overflow");
+//         }
+//         this.dados[this.topo] = elemento;
+//         this.topo++;
+//     }
+//     pop() {
+//         if (this.isEmpty()) {
+//             throw new Error("Stack underflow");
+//         }
+//         this.topo--;
+//     }
+//     top() {
+//         if (!this.isEmpty()) {
+//             return this.dados[this.topo - 1];
+//         }
+//     }
+//     isEmpty() {
+//         return this.length() === 0;
+//     }
+//     isFull() {
+//         return this.length() === this.size;
+//     }
+//     toString() { }
+//     clear() {
+//         this.topo = 0;
+//     }
+//     length() {
+//         return this.topo;
+//     }
+// }
+// export default Pilha;
+
+
+
+// //Lista 1: Questão 1
+// class Pilha {
+//     constructor () {
+//         this.itens = [];
+//     }
+
+//     //Adiciona elemento no topo
+//     push(elemento) {
+//         this.itens.push(elemento);
+
+//     }
+
+//     //Remove elementos
+//     pop(){
+//         if (this.isEmpty()){
+//             throw new Error ("Pilha vazia");
+//         }
+//         return this.itens.pop();
+//     }
+
+//     //Verifica se tá vazia
+//     isEmpty(){
+//         return this.itens.length === 0;
+//     }
+// }
+
+// export default Pilha;
+
+// export function inverte(palavra){
+//     const pilha = new Pilha();
+
+//     //Adicionar caractere da palavra na pilha
+//     for (let i = 0; i <palavra.length; i++){
+//         pilha.push(palavra[i]);
+//     }
+
+//     //Palavra invertida removendo os elementos da pilha
+//     let palavraInvertida = "";
+//     while (!pilha.isEmpty()){
+//         palavraInvertida += pilha.pop();
+//     }
+
+//     return palavraInvertida;
+// }
+
+// ____________________________________________
+
+// //Lista 1: Questão 2
+// class DuasPilhas{
+//     constructor(tamanho){
+//         this.vetor = new Array(tamanho);
+//         this.topoA = -1; 
+//         this.topoB = tamanho;
+//     }
+
+//     eVaziaA(){
+//         return this.topoA === -1;
+//     }
+
+//     eVaziaB(){
+//         return this.topoB === this.vetor.length;
+//     }
+    
+//     empilhaA(elemento){
+//         if(this.topoA + 1 === this.topoB){
+//             throw new Error ('Pilha Cheia');
+//         }
+//         this.topoA++; //Anda o topo pra frente
+//         this.vetor[this.topoA] = elemento;
+//     }
+
+//     empilhaB(elemento){
+//         if(this.topoB - 1 === this.topoA){
+//             throw new Error ('Pilha Cheia');
+//         }
+//         this.topoB--; //Anda o topo pra trás
+//         this.vetor[this.topoB] = elemento;
+//     }
+
+//     desempilhaA(){
+//         if(this.eVaziaA()){
+//             throw new Error ('Erro: Pilha vazia');
+//             return null;
+//         }
+//         const elemento = this.vetor[this.topoA];
+//         this.topoA--;
+//         return elemento;
+//     }
+    
+//     desempilhaB(){
+//         if(this.eVaziaB()){
+//             throw new Error ('Erro: Pilha vazia');
+//             return null;
+//         }
+//         const elemento = this.vetor[this.topoB];
+//         this.topoB++;
+//         return elemento;
+//     }
+// }
+
+// export default DuasPilhas;
+
+// // ____________________________________________
+
+// //Lista 1: Questão 3
